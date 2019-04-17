619d243aa766995e65f306a9bbec37553de7c0d8
JSKarras <31581146+JSKarras@users.noreply.github.com>
2019-04-17 16:20:33 -0700

    Fixed initialize_svd.cpp
    smh chard's local filepaths

--- initialize_svd_data.py
+++ initialize_svd_data.py
@@ -1,32 +1,32 @@
-# Loads training data into base.npy
+# Loads training data into base.npy 
 
 import numpy as np
 import pandas as pd
 
 ''' 1. Load data '''
-df = pd.read_table('E:\\Documents\\Caltech\\CS156\\mu\\all.dta', delim_whitespace=True, header=None)
+df = pd.read_table('all.dta', delim_whitespace=True,header=None)
 df = np.array(df)
 
-idx = pd.read_table('E:\\Documents\\Caltech\\CS156\\mu\\all.idx', delim_whitespace=True, header=None)
+idx = pd.read_table('all.idx', delim_whitespace=True,header=None)
 idk = np.array(idx)
 
-# Increment movie/user down by 1 if you need
-#for i in df:
-#    i[0] -= 1
-#    i[1] -= 1
-
+# Initialize dataframe 
+for i in df: 
+    i[0] -= 1
+    i[1] -= 1
+    
 print("Finished loading data.\n")
 
 ''' 2. Get indices for training and testing data '''
 
 # write to .txt files for c++ usage
-base = open("E:\\Documents\\Caltech\\CS156\\base.dta", "w")
-qual = open("E:\\Documents\\Caltech\\CS156\\qual.dta", "w")
+base = open("base.txt", "w")
+qual = open("qual.txt", "w")
 for i in range(len(idx)):
     if idk[i][0] == 1:
-        base.write(str(df[i][0]) + " " + str(df[i][1]) + " " + str(df[i][2]) + " " + str(df[i][3]) + "\n")
+        base.write(str(df[i][0]) + " " + str(df[i][1]) + " " + str(df[i][2]) +" " + str(df[i][2]) + "\n");
     if idk[i][0] == 5:
-        qual.write(str(df[i][0]) + " " + str(df[i][1]) + " " + str(df[i][2]) + "\n")
-
+        qual.write(str(df[i][0]) + " " + str(df[i][1]) + "\n")
+        
 base.close()
 qual.close()
