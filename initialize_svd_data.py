7e30d74b45ef5a19801310206bc4a6d3379f4ba2
JSKarras <31581146+JSKarras@users.noreply.github.com>
2019-04-30 19:14:14 -0700

    Update initialize_svd_data.py

--- initialize_svd_data.py
+++ initialize_svd_data.py
@@ -24,7 +24,7 @@ base = open("base.txt", "w")
 qual = open("qual.txt", "w")
 for i in range(len(idx)):
     if idk[i][0] == 1:
-        base.write(str(df[i][0]) + " " + str(df[i][1]) + " " + str(df[i][2]) +" " + str(df[i][2]) + "\n");
+        base.write(str(df[i][0]) + " " + str(df[i][1]) + " " + str(df[i][2]) +" " + str(df[i][3]) + "\n");
     if idk[i][0] == 5:
         qual.write(str(df[i][0]) + " " + str(df[i][1]) + "\n")
         
